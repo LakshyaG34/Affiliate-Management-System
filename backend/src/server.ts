4913c43989ac3406d8f2c4070f697db1ca3eddc3
Lakshya <lakshyagoyal34@gmail.com>
2026-08-01 14:08:00 +0530

    Debugged Prisma

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,19 @@
 import express from "express";
+import prisma from "@/lib/prisma";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (_req, res) => {
-  res.send("Affiliate Management API");
+app.get("/", async (_req, res) => {
+  const users = await prisma.user.findMany();
+
+  res.json(users);
 });
 
-const PORT = process.env.PORT || 5000;
+
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
