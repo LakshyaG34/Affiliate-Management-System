06f6411e5d62e6906b314b65b852d22c6ea7fd7b
Lakshya <lakshyagoyal34@gmail.com>
2026-08-05 15:01:54 +0530

    changed frontend url name

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://affiliate-os.lakshyagoyal.com"],
     credentials: true,
   })
 );
